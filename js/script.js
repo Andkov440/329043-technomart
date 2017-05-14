d0b7dc6a209e37cb3b9106207eadbe7a4937a4cd
Andkov440 <andkov440@mail.ru>
2017-05-14 11:51:29 +0400

    Исправления

--- js/script.js
+++ js/script.js
@@ -2,17 +2,13 @@ var cart_link = document.querySelectorAll(".buy");
 var cart_popup = document.querySelector(".cart-modal");
 var cart_close = document.querySelector(".modal-content-close");
 var cart = document.querySelector(".cart-btn");
-
 var bookmarks = document.querySelector(".bookmarks-btn");
 var bookmark = document.querySelectorAll(".bookmark");
 var bookmarks_count = document.querySelector(".bookmarks-count");
 var orders_count = document.querySelector(".orders-count");
-
-
 var write_link = document.querySelector(".feedback");
 var write_popup = document.querySelector(".write-us");
 var write_close = document.querySelector(".modal-content-close");
-
 var count_сart = 0;
 var count_bookmarks = 0;
 
@@ -22,7 +18,6 @@ for(var i=0; i<cart_link.length; i++) {
     cart_popup.classList.add("cart-modal-show");
     cart.classList.add("cart-btn-full");
     orders_count.innerText = ++count_сart;
-
   });
 }
 
@@ -39,11 +34,9 @@ cart_close.addEventListener("click", function(event) {
   cart_popup.classList.remove("cart-modal-show");
 });
 
-
-
 write_link.addEventListener("click", function(event) {
-    event.preventDefault();
-    write_popup.classList.add("write-us-show");
+  event.preventDefault();
+  write_popup.classList.add("write-us-show");
 });
 
 write_close.addEventListener("click", function(event) {
